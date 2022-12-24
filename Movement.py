2179731a78b640130c6bff480b85d146ea99109b
himanshu2k15 <himanshu.techguru@gmail.com>
2022-12-24 11:26:44 +0530

    Added Movement file

---
+++ Movement.py
@@ -0,0 +1,13 @@
+import pyautogui,time
+num_sec=10
+print("Goint to Start")
+x,y=pyautogui.size()
+for i in range(10000):
+    time.sleep(5) 
+    pyautogui.moveTo(1,1,duration=num_sec)
+    pyautogui.moveTo(x-1,1,duration=num_sec)
+    pyautogui.moveTo(x-10,y-10,duration=num_sec)
+    pyautogui.moveTo(1,y-1,duration=num_sec)
+    pyautogui.moveTo(1,1,duration=num_sec)
+    time.sleep(5)
+    print('Moving Frame Time- ',i)
